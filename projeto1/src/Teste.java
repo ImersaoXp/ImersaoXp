35eef42b3437f5b2a21b2acf5829fe64d177eae8
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-12-02 09:40:34 -0200

    .

--- projeto1/src/Teste.java
+++ projeto1/src/Teste.java
@@ -1,4 +1,13 @@
 
 public class Teste {
 
+	public static void main(String[] args) throws InterruptedException {
+		while (true) {
+			System.out.println();
+			System.out.println();
+			System.out.println("   " + System.currentTimeMillis());
+			Thread.sleep(300);
+		}
+	}
+	
 }

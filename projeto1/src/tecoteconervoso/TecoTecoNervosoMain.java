e5fbff324778546baaf6ce4f9f58ed9eab05ae18
unknown <linux@.(none)>
2010-12-02 15:47:15 +0430

    a

--- projeto1/src/tecoteconervoso/TecoTecoNervosoMain.java
+++ projeto1/src/tecoteconervoso/TecoTecoNervosoMain.java
@@ -1,18 +1,29 @@
 package tecoteconervoso;
 
+import base.CapturadorDeTecla;
+
 
 public class TecoTecoNervosoMain {
 
 	public static void main(String[] args) throws InterruptedException {
 		
 		TecoTecoNervoso tn = new TecoTecoNervoso();
+		CapturadorDeTecla capturador = new CapturadorDeTecla();
 		String[] tela;
 		
 		while(true) 
 		{
+			System.out.println(capturador.getKeyCode());
 			tela = tn.tela();
-			for (int x = 0; x < 5; x++) {
-				System.out.println(tela[x]);
+			for (int linha = 0; linha < 5; linha++) {
+				if(capturador.getKeyCode() == 39){
+					tn.direita();					
+				}else if (capturador.getKeyCode() == 37){
+					tn.esquerda();
+				}
+				capturador.setKeyCode(0);
+				System.out.println(tela[linha]);
+				
 			}
 			Thread.sleep(100);
 		}

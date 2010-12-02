d01eed3e79e598376a4d01ea15090811a21846fa
unknown <linux@.(none)>
2010-12-02 14:48:51 +0430

    v1

--- projeto1/src/tecoteconervoso/TecoTecoNervoso.java
+++ projeto1/src/tecoteconervoso/TecoTecoNervoso.java
@@ -1,10 +1,27 @@
 package tecoteconervoso;
 
+import java.util.Arrays;
+
 public class TecoTecoNervoso {
-
-	public String[] tela() {
-		// TODO Auto-generated method stub
-		return null;
+	private char[][] tela = new char[5][5];
+	
+	
+	
+	public void inicializarJogo(){
+		Arrays.fill(tela, ' ');
+		tela[5][3] = '^';
 	}
-
+	
+//	private void esquerda
+	
+	
+	public String[] tela() {
+		inicializarJogo();
+		String[] saida = new String[5];     
+		for (int i = 0; i < 5; i++) {
+			saida[i] = new String(tela[i]); 
+		}
+		return saida;
+	}
+	
 }

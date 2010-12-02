285aa45d4bd5f42626fb2bcad75db56f5ef84ab9
unknown <linux@.(none)>
2010-12-02 21:28:51 +0430

    .

---
+++ projeto1/src/tecoteconervoso/testes/AviaoTiroTest.java
@@ -0,0 +1,37 @@
+package tecoteconervoso.testes;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import tecoteconervoso.TecoTecoNervoso;
+
+
+public class AviaoTiroTest {
+	@Ignore
+	@SuppressWarnings("deprecation")
+	@Test public void aviao() {
+		TecoTecoNervoso jogo = new TecoTecoNervoso();
+		jogo.tela();
+		jogo.atira();
+		String[] telaEsperada = new String[] {
+				"     ",
+				"     ",
+				"     ",
+				"  !  ",
+				"  ^  ",
+		};
+		Assert.assertEquals(telaEsperada, jogo.tela());
+		
+		jogo.tela();
+		jogo.tela();
+		telaEsperada = new String[] {
+				"     ",
+				"  !  ",
+				"     ",
+				"     ",
+				"  ^  ",
+		};
+		Assert.assertEquals(telaEsperada, jogo.tela());
+	}
+}

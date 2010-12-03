21356c5aa4975a6a413bf92a0fd099e34de0ec30
unknown <linux@.(none)>
2010-12-03 16:11:36 +0430

    .

---
+++ projeto1/src/tecoteconervoso/Aviao.java
@@ -0,0 +1,17 @@
+package tecoteconervoso;
+
+public class Aviao {
+	public int posicaoX = 2;
+	
+	public void direita() {
+		posicaoX++;		
+	}
+
+	public void esquerda() {
+		posicaoX--;		
+	}
+	
+	public void desenha(char[][] tela) {
+		tela[4][posicaoX] = '^';
+	}
+}
